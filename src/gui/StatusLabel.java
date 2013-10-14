b0f1adc60a6d548cdf42480843b9b979b270f513
Axel Mårtensson <axel.martensson@hotmail.com>
2013-10-15 00:17:51 +0200

    found a usage for CurrentLabel

--- src/gui/StatusLabel.java
+++ src/gui/StatusLabel.java
@@ -10,6 +10,7 @@ public class StatusLabel extends ColoredLabel implements Observer {
     }
 
     public void update(Observable observable, Object object) {
+    	//TODO add behavior
         setText("");
     }
 }
