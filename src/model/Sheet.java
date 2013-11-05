9e790f8c0372a6086ff4d366ff8931014f078ad1
Kasper HunkeN Bratz <dat12kbr@student.lu.se>
2013-11-05 11:51:55 +0100

    Added function to sheet

--- src/model/Sheet.java
+++ src/model/Sheet.java
@@ -2,10 +2,12 @@ package model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Observable;
 
+import util.XLException;
 import expr.Environment;
 
-public class Sheet implements Environment{
+public class Sheet extends Observable implements Environment{
 	private Map<String, Slot> slots;
 	
 	public Sheet() {
@@ -19,7 +21,11 @@ public class Sheet implements Environment{
 	@Override
 	public double value(String name) {
 //		try{
-		double value = slots.get(name).value(this);
+		Slot slot = slots.get(name);
+		if (slot == null){
+			throw new XLException("Cannot refer to empty slot: "+name);
+		}
+		double value = slot.value(this);
 //		} catch(XLEvalException e) { 
 //			e.setAddress(name); // slap on an address
 //			throw e; // and let somebody else worry about it
@@ -30,6 +36,22 @@ public class Sheet implements Environment{
 	public void putSlot(String name, String content){
 			Slot slot = SlotFactory.build(content);
 			slots.put(name, slot);
+			setChanged();
+			notifyObservers();
+	}
+	
+	public void removeSlot(String name){
+		slots.remove(name);
+	}
+	
+	public String slotRepresentation(String name){
+		Slot slot = slots.get(name);
+		return slot.representation(this);
+	}
+	
+	public String editorRepresentation (String name){
+		Slot slot = slots.get(name);
+		return slot.toString();
 	}
 
 }
